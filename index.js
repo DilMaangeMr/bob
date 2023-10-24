fe1a6192508dacb458b786e03c51b87df3612220
Dev <rajs0439@gmail.com>
2023-10-24 17:06:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById("root"))
+
+// ReactDOM.render(<p>Hello World !</p>, document.getElementById("root"))
+
+
+const element = <p>Hello World !</p>;
+ReactDOM.render(element, document.getElementById("root"));
